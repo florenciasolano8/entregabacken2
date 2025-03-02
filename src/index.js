a3977e110aeaff8e2aeb8b3720d3c4aecbdf0ff5
florencia solano <solanoflorencia16@gmail.com>
2025-03-02 15:34:29 -0300

    entrega final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config(); 
 import express from "express";
 import connectDB from "./config/database.js";
 import cookieParser from "cookie-parser";
@@ -6,27 +8,24 @@ import userRouter from "./routes/user.routes.js";
 import { initializePassport } from "./config/passport.config.js"; 
 import passport from "passport";
 
-
 const app = express();
-app.set("PORT", 3000);
-const uri = "mongodb://127.0.0.1:27017/user";
+app.set("PORT", process.env.PORT || 3000);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(passport.initialize());
 
-
 app.use("/api/users", userRouter); 
-
-initializePassport(); 
+initializePassport();
 app.get("/", (req, res) => {
   res.json({ title: "Home Page!!" });
 });
 app.use("/api/session", sessionRouter);
 
-//listeners
-connectDB(uri);
+connectDB(process.env.MONGODB_URI); 
+
+// Inicia el servidor
 app.listen(app.get("PORT"), () => {
-  console.log(`Server on port http://localhost:${app.get("PORT")}`);
+  console.log(`Server running on http://localhost:${app.get("PORT")}`);
 });
